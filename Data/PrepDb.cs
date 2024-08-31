bba2d77475f1b92ff1d5abbfb0c01d5061b7f1f2
olliep24 <odpetrick@berkeley.edu>
2024-08-30 17:09:18 -0700

    Basic project set up on .NET 8. Platforms api endpoint working

---
+++ Data/PrepDb.cs
@@ -0,0 +1,52 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+#pragma warning disable CS8604 // Possible null reference argument.
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+#pragma warning restore CS8604 // Possible null reference argument.
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Seeding Data...");
+
+                context.Platforms.AddRange(
+                    new Platform()
+                    {
+                        Name = "Dot Net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "SQL Server Express",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Kubernetes",
+                        Publisher = "Cloud Native Computer Foundation",
+                        Cost = "Free"
+                    }
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Data already present");
+            }
+        }
+    }
+}
---
+++ Dtos/PlatformReadDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PlatformService.Dtos
+{
+    public class PlatformReadDto
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Publisher { get; set; }
+
+        public required string Cost { get; set; }
+    }
+}
